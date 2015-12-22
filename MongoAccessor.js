fdaa9ada8e9cde2fd01508c51d14219a6933e45b
hurstj <jason.hurst@cdk.com>
2015-12-21 17:48:44 -0800

    Updated MongoAccessor to have better compartmentalization of the event emitter.

--- MongoAccessor.js
+++ MongoAccessor.js
@@ -13,33 +13,38 @@ module.exports = {
 function MongoAccessor() {
     
     var MongoURL;
-    var fnPrivate = new Object(); //private function storage
+    //var fnPrivate = new Object(); //private function storage
     var oEmitter = new EventEmitter(); //need to encapsolate this better
-
-    this.init = function (strMongoConnectionURL) { 
-        MongoURL = strMongoConnectionURL;    
+    
+    this.init = function (strMongoConnectionURL) {
+        MongoURL = strMongoConnectionURL;
     };
+    
     this.GetProjectBoxList = function () {
+        var fnPrivate = new MongoPrivates(MongoURL, oEmitter);
+        
         fnPrivate.MongoConnect(fnPrivate.GetProjectBoxListCallback);
+        
         return oEmitter;
     };
     
-    
-    
-    /*****************Private functions****************/
-    fnPrivate.MongoConnect = function (fnCallback) {
+}
+
+/*****************Private functions****************/
+function MongoPrivates(MongoURL, oEmitter) {
+    var MongoURL = MongoURL;
+    var oEmitter = oEmitter;
+
+    this.MongoConnect = function (fnCallback) {
         MongoClient.connect(MongoURL, fnCallback);
     };
-    fnPrivate.GetProjectBoxListCallback = function (err, db) {
+
+    this.GetProjectBoxListCallback = function (err, db) {
         assert.equal(null, err); //check good connection to database
         
         console.log('Retrieving Record....\n');
         
         db.collection("ProjectBoxList").findOne({ _id: 'CurrentProjectBoxList' }, function (err, document) {
-            //assert.equal(null, err); //throw if there is an error
-            //response.end(JSON.stringify(document));
-            //var oEmitter = new EventEmitter();
-            
             console.log("Found data to return.");
             
             if (err != null)
